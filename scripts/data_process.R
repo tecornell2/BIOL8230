714d9c70350704d3beffaa33b97fb8b4b590e162
Taryn Cornell <77257369+tecornell2@users.noreply.github.com>
2026-09-24 21:15:56 -0400

    Update header and add comments in data_process.R

---
+++ scripts/data_process.R
@@ -0,0 +1,11 @@
+###########################
+##### DATA PROCESSING #####
+###########################
+
+### import packages
+
+### import data
+
+# excel sheet
+# transform to df
+# check data for missing values
